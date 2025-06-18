8264bf65cefc36053883ed88838b9ecaa83bb706
Santiago Lacki <santiagolacki@gmail.com>
2025-06-18 13:26:58 -0300

    Se agrego la resolucion del pto1 parte 3

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import HolaMundo from "./Components/HolaMundo"
 
 function App() {
 
-  const mensaje = "My Friend!"
+  const mensaje = "My Friend"
 
   return (
       <HolaMundo mensajeProps={mensaje}/>
--- src/Components/HolaMundo.jsx
+++ src/Components/HolaMundo.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
 
 const HolaMundo = (props) => {
+
+  const [parentesis, setParentesis] = useState("");
+
+  const mostrarParentesis = () => {
+    setParentesis(parentesis + '(From changed state)')
+  }
+
   return (
     <div>
-      <h1>HEllO {props.mensajeProps} </h1>
+      <h1>HEllO {props.mensajeProps} {parentesis} !</h1>
+      <button type="button" onClick={mostrarParentesis}>Click Me!</button>
     </div>
   );
 };
